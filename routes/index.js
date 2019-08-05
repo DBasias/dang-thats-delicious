ccd833e802495ea84d2ec9124f0898127584213b
DBasias <dbasiadimas@hotmail.com>
2019-08-05 18:43:21 +0300

    created route to search stores by location

--- routes/index.js
+++ routes/index.js
@@ -57,5 +57,6 @@ router.post(
 */
 
 router.get('/api/v1/search', catchErrors(storeController.searchStores));
+router.get('/api/v1/stores/near', catchErrors(storeController.mapStores));
 
 module.exports = router;
