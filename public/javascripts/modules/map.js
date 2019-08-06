80d147a3984067274cb83ce347280d1e7390acff
DBasias <dbasiadimas@hotmail.com>
2019-08-06 19:39:25 +0300

    created and added markers on map

--- public/javascripts/modules/map.js
+++ public/javascripts/modules/map.js
@@ -1,16 +1,43 @@
 import axios from 'axios';
+import { $ } from '../modules/bling';
 
 const mapOptions = {
   center: { lat: 43.2, lng: -79.8 },
-  zoom: 8
+  zoom: 10
 };
 
-function loadPlaces(map, lat = 43.2, lng = -79.8) {}
+function loadPlaces(map, lat = 43.2, lng = -79.8) {
+  axios.get(`api/v1/stores/near?lat=${lat}&lng=${lng}`).then(res => {
+    const places = res.data;
+    if (!places.length) {
+      alert('No places found!');
+      return;
+    }
+
+    const bounds = new google.maps.LatLngBounds();
+
+    const markers = places.map(place => {
+      const [placeLng, placeLat] = place.location.coordinates;
+      const position = { lat: placeLat, lng: placeLng };
+      bounds.extend(position);
+      const marker = new google.maps.Marker({ map, position });
+      marker.place = place;
+      return marker;
+    });
+
+    map.setCenter(bounds.getCenter());
+    map.fitBounds(bounds);
+  });
+}
 
 function makeMap(mapDiv) {
   if (!mapDiv) return;
 
   const map = new google.maps.Map(mapDiv, mapOptions);
+  loadPlaces(map);
+
+  const input = $('[name="geolocate"]');
+  const autocomplete = new google.maps.places.Autocomplete(input);
 }
 
 export default makeMap;
